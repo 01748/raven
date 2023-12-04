7e81d670a020aa62e3bf310c1ac61ebac295589e
Ashik <ashikur1993@gmail.com>
2023-12-04 16:49:05 +0600

    com

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,75 +1,81 @@
-/**
- * @Script js for (Template/Project Name)
- *
- * @project     - Project Name
- * @author      - 
- * @created_by  - Asraful Hoque
- * @created_at  - 
- * @modified_by -
- */
-
-
-/**
- * ========================================================
- * this function execute when window properly loaded
- * ===========================================================
- */
-
-$(window).on('load', function () {
-
-    // code should be execute here
-
-});
-
-
-
-/**
- * ========================================================
- * this function execute when DOM element ready 
- * ===========================================================
- */
-
-$(document).ready(function () {
-
-    // testimonial-active
-    $(function () {
-        if ($('.testimonials-active').length) {
-            $(".testimonials-active").owlCarousel({
-                items: 3,
-                margin: 30,
-                nav: true,
-                loop: true,
-                autoplay: true,
-                autoplayTimeout: 2500,
-                animateOut: 'fadeOut',
-                smartSpeed: 2500,
-                navText: ["<img src='assets/img/arrow-left.svg' class='img-fluid' />", "<img src='assets/img/arrow-right-s.svg' class='img-fluid' />"],
-                dots: false,
-                autoplayHoverPause: true,
-                responsive: {
-                    0: {
-                        items: 1
-                    },
-                    767: {
-                        items: 2
-                    },
-                    1200: {
-                        items: 3
-                    }
+class countUp {
+    constructor(el) {
+        this.el = el;
+        this.setVars();
+        this.init();
+    }
+  
+    setVars() {
+        this.number = this.el.querySelectorAll("[data-countup-number]");
+        this.observerOptions = { root: null, rootMargin: "0px 0px", threshold: 0 };
+        this.observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                const end = parseFloat(entry.target.dataset.countupNumber.replace(/,/g, ""));
+                const decimals = this.countDecimals(end);
+                if (entry.isIntersecting) {
+                    this.iterateValue(entry.target, end, decimals);
                 }
             });
+        }, this.observerOptions);
+    }
+  
+    init() {
+        if (this.number.length > 0) {
+            this.number.forEach((el) => {
+                this.observer.observe(el);
+            });
         }
-    });
-
-
-
-    // wow js init
-    $(function () {
-        var wow = new WOW({
-            animateClass: 'animated',
-            mobile: true
+    }
+  
+    iterateValue(el, end, decimals) {
+        const start = 0;
+        const duration = 2500;
+        let startTimestamp = null;
+  
+        const step = (timestamp) => {
+            if (!startTimestamp) startTimestamp = timestamp;
+            const elapsedPercent = (timestamp - startTimestamp) / duration;
+            const easedProgress = Math.min(this.easeOutQuint(elapsedPercent), 1);
+            let interimNumber = Math.abs(easedProgress * (end - start) + start);
+            el.innerHTML = this.formatNumber(interimNumber, decimals);
+            if (easedProgress < 1) {
+                window.requestAnimationFrame(step);
+            }
+        };
+  
+        // requestAnimationFrame returns DOMHighResTimeStamp as a callback (used as timestamp)
+        window.requestAnimationFrame(step);
+    }
+  
+    easeOutQuad(x) {
+        return 1 - Math.pow(1 - x, 3);
+    }
+  
+    easeOutQuint(x) {
+        return 1 - Math.pow(1 - x, 5);
+    }
+  
+    countDecimals(val) {
+        if (Math.floor(val) === val) return 0;
+        return val.toString().split(".")[1].length || 0;
+    }
+  
+    formatNumber(val, decimals) {
+        return val.toLocaleString("en-US", {
+            minimumFractionDigits: decimals,
+            maximumFractionDigits: decimals
         });
-        wow.init();
+    }
+  }
+  
+  // Simplified version of Viget dynamic modules to attach instances for this demo
+  // https://www.viget.com/articles/how-does-viget-javascript/
+  // You CAN use this pattern, but it's single purpose right now
+  
+  const dataModules = [...document.querySelectorAll('[data-module="countup"]')];
+  
+  dataModules.forEach((element) => {
+    element.dataset.module.split(" ").forEach((moduleName) => {
+        new countUp(element);
     });
-
-});
+  });
